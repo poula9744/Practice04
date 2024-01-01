e6ba54e4470ba393d25167d4a675b52cb7aa9d9b
poula9744 <poula9744@naver.com>
2024-01-01 17:55:47 +0900

    수정

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -1,14 +1,11 @@
 package com.javaex.practice;
 
-import java.util.Arrays;
-
 public class Ex08 {
 
 	public static void main(String[] args) {
 		
 		//배열크기
 		int[] lottoNoArray = new int[6];
-		int[] lotto = new int[6];
 		
 		for(int i = 0; i<lottoNoArray.length; i++) {
 			//랜덤함수
@@ -20,16 +17,13 @@ public class Ex08 {
 					i--;
 					 break;
 				}
-			//중복제거
-			
-				
 			}
-			
-			//출력
-			System.out.print(lottoNoArray[i]+ "  ");
-			
 		}
-
+		
+		//출력
+		System.out.print(lottoNoArray[0]+ "  " + lottoNoArray[1]+ "  " + lottoNoArray[2]+ "  " 
+					     + lottoNoArray[3]+ "  "+ lottoNoArray[4]+ "  " + lottoNoArray[5]);
+		
 	}
 
 }
